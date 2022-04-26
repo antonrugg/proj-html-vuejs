ed8298001595363923876dd69ef473ec88bf1074
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-26 13:57:08 +0200

    init scaffolding

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    FOOTER
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    HEADER
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
