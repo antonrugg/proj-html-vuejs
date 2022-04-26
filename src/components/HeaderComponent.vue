7d7fdb144e9a2aa8fbf0510e2f969f37f7f191fc
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-26 16:09:37 +0200

    imported all icons

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    
     <HeaderNavBarComponent :headerLinks="headerLinks"/>
   </header>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// icons
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faRectangleList } from '@fortawesome/free-solid-svg-icons'
+
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons'
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+library.add(faCartShopping, faRectangleList, faInstagram, faTwitter, faFacebookF, faPinterestP)
+
 Vue.config.productionTip = false
 
 new Vue({
